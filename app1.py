a906a0986e8d82d523ce5a06f2f0f57bc737c40d
salehpythoncourse <111654329+salehpythoncourse@users.noreply.github.com>
2022-08-20 20:18:30 +0300

    Add files via upload

---
+++ app1.py
@@ -0,0 +1,11 @@
+import math_helpers
+
+print(math_helpers.multiply(10, 20))
+print(math_helpers.multiply(10, 20, 30, 40))
+
+print('------')
+
+print(math_helpers.avg(10, 12, 14, 20))
+print(math_helpers.avg())
+
+print(help(math_helpers))
